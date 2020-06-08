f181b729cc8d4a71631834dd5fb09b6bdd4e866a
Demetri Sebastian Lopez <dsl418@nyu.edu>
2020-06-07 22:38:58 -0400

    Project 1

---
+++ P1/SDLProject/SDLProject/main.cpp
@@ -0,0 +1,130 @@
+#define GL_SILENCE_DEPRECATION
+
+#ifdef _WINDOWS
+#include <GL/glew.h>
+#endif
+
+#define GL_GLEXT_PROTOTYPES 1
+#include <SDL.h>
+#include <SDL_opengl.h>
+#include "glm/mat4x4.hpp"
+#include "glm/gtc/matrix_transform.hpp"
+#include "ShaderProgram.h"
+
+#define STB_IMAGE_IMPLEMENTATION
+#include "stb_image.h"
+
+SDL_Window* displayWindow;
+bool gameIsRunning = true;
+
+ShaderProgram program;
+glm::mat4 viewMatrix, projectionMatrix, playerMatrixID, duckMatrix;
+
+GLuint playerTextureID, duckTexture;
+
+GLuint LoadTexture(const char* filePath) {
+    int w, h, n;
+    unsigned char* image = stbi_load(filePath, &w, &h, &n, STBI_rgb_alpha);
+    
+    if (image == NULL) {
+        std::cout << "Unable to load image. Make sure the path is correct\n"; assert(false);
+    }
+    
+    GLuint textureID;
+    glGenTextures(1, &textureID);
+    glBindTexture(GL_TEXTURE_2D, textureID);
+    glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, w, h, 0, GL_RGBA, GL_UNSIGNED_BYTE, image);
+    
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
+    
+    stbi_image_free(image);
+    return textureID;
+    }
+
+void Initialize() {
+    SDL_Init(SDL_INIT_VIDEO);
+    displayWindow = SDL_CreateWindow("Textured!", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 640, 480, SDL_WINDOW_OPENGL);
+    SDL_GLContext context = SDL_GL_CreateContext(displayWindow);
+    SDL_GL_MakeCurrent(displayWindow, context);
+    
+#ifdef _WINDOWS
+    glewInit();
+#endif
+    
+    glViewport(0, 0, 640, 480);
+    
+    program.Load("shaders/vertex_textured.glsl", "shaders/fragment_textured.glsl");
+    
+    viewMatrix = glm::mat4(1.0f);
+    projectionMatrix = glm::ortho(-5.0f, 5.0f, -3.75f, 3.75f, -1.0f, 1.0f);
+    playerMatrixID = glm::mat4(1.0f);
+    duckMatrix = glm::mat4(1.0f);
+    
+    program.SetProjectionMatrix(projectionMatrix);
+    program.SetViewMatrix(viewMatrix);
+    //program.SetColor(1.0f, 0.0f, 0.0f, 1.0f);
+    
+    glUseProgram(program.programID);
+    
+    glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
+    
+    playerTextureID = LoadTexture("ctg.png");
+    duckTexture = LoadTexture("jump.png");
+}
+
+void ProcessInput() {
+    SDL_Event event;
+    while (SDL_PollEvent(&event)) {
+        if (event.type == SDL_QUIT || event.type == SDL_WINDOWEVENT_CLOSE) {
+            gameIsRunning = false;
+        }
+    }
+}
+
+void Update() {
+    playerMatrixID = glm::translate(playerMatrixID, glm::vec3(0.0f, -0.01f, 0.0f));
+    duckMatrix = glm::rotate(duckMatrix, glm::radians(45.0f), glm::vec3(0.0f, 0.0f, 1.0f));
+    duckMatrix = glm::scale(duckMatrix, glm::vec3(1.005f, 1.005f, 0.0f)); // lol I dont know how to make it stop
+}
+
+void Render() {
+    glClear(GL_COLOR_BUFFER_BIT);
+    
+    float vertices[] = { -0.5, -0.5, 0.5, -0.5, 0.5, 0.5, -0.5, -0.5, 0.5, 0.5, -0.5, 0.5 };
+    float texCoords[] = { 0.0, 1.0, 1.0, 1.0, 1.0, 0.0, 0.0, 1.0, 1.0, 0.0, 0.0, 0.0 };
+    
+    glVertexAttribPointer(program.positionAttribute, 2, GL_FLOAT, false, 0, vertices);
+    glEnableVertexAttribArray(program.positionAttribute);
+    glVertexAttribPointer(program.texCoordAttribute, 2, GL_FLOAT, false, 0, texCoords);
+    glEnableVertexAttribArray(program.texCoordAttribute);
+    
+    program.SetModelMatrix(playerMatrixID);
+    glBindTexture(GL_TEXTURE_2D, playerTextureID);
+    glDrawArrays(GL_TRIANGLES, 0, 6);
+    
+    program.SetModelMatrix(duckMatrix);
+    glBindTexture(GL_TEXTURE_2D, duckTexture);
+    glDrawArrays(GL_TRIANGLES, 0, 6);
+    
+    glDisableVertexAttribArray(program.positionAttribute);
+    glDisableVertexAttribArray(program.texCoordAttribute);
+    SDL_GL_SwapWindow(displayWindow);
+}
+
+void Shutdown() {
+    SDL_Quit();
+}
+
+int main(int argc, char* argv[]) {
+    Initialize();
+    
+    while (gameIsRunning) {
+        ProcessInput();
+        Update();
+        Render();
+    }
+    
+    Shutdown();
+    return 0;
+}
